486fc11f82e26955428974bdf007766a676f78b2
Frost15777 <di.yu.mk@gmail.com>
2024-02-09 09:48:33 +0100

    main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux-items/store';
+import { store } from 'redux-items/store';
 import App from 'components/App';
 import './index.css';
 
